6d4b3326574cf2d6f8737ca65752b9f8c5dc4852
Devin Jay San Nicolas <dds324@nau.edu>
2025-11-05 15:52:07 -0700

    fix sql

--- dbsql.sql
+++ dbsql.sql
@@ -2,8 +2,7 @@ CREATE DATABASE BookDB;
 CREATE TABLE Users (
     username VARCHAR(50) PRIMARY KEY,
     displayName VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Books (
@@ -18,10 +17,9 @@ CREATE TABLE Books (
 
 CREATE TABLE Reviews (
     reviewID SERIAL PRIMARY KEY,
-    bookID INT REFERENCES Books(bookID),
-    author VARCHAR(50) REFERENCES Users(username),
+    bookID INT REFERENCES Books(bookID) ON DELETE CASCADE,
+    author VARCHAR(50) REFERENCES Users(username) ON DELETE CASCADE,
     reviewText TEXT NOT NULL,
     reviewTitle VARCHAR(100) NOT NULL,
-    rating INT NOT NULL,
-    DELETE ON CASCADE
+    rating INT NOT NULL
 );
